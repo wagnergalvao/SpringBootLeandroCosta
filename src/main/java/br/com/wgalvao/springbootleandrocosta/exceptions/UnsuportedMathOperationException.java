338177e44f100e8387650420e4abb932a99b4d8c
Wagner Galvão <galvao.wagner@outlook.com.br>
2022-08-05 02:13:09 -0300

    class 065 refactored ResponseEntityExceptionHandler class

--- src/main/java/br/com/wgalvao/springbootleandrocosta/exceptions/UnsuportedMathOperationException.java
+++ src/main/java/br/com/wgalvao/springbootleandrocosta/exceptions/UnsuportedMathOperationException.java
@@ -1,9 +1,6 @@
 package br.com.wgalvao.springbootleandrocosta.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+//@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class UnsuportedMathOperationException extends RuntimeException {
 
     public UnsuportedMathOperationException(String message) {
--- src/main/java/br/com/wgalvao/springbootleandrocosta/exceptions/handler/RestExceptionHandler.java
+++ src/main/java/br/com/wgalvao/springbootleandrocosta/exceptions/handler/RestExceptionHandler.java
@@ -1,48 +1,74 @@
 package br.com.wgalvao.springbootleandrocosta.exceptions.handler;
 
-import java.util.Date;
-import java.util.HashMap;
-
-import javax.xml.bind.ValidationException;
-
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConversionException;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.web.HttpMediaTypeException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import com.fasterxml.jackson.core.JsonParseException;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
-    protected ResponseEntity<Object> handleRestClientResponseExcption(
-            RestClientResponseException ex, HttpHeaders headers,
-            HttpStatus status, WebRequest request) {
-
+    protected ResponseEntity<Object> handleHttpStatusCodeException(
+            Exception ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
         return buildResponseEntity(ex, headers, status, request);
     }
 
-    protected ResponseEntity<Object> handleGenericException(
-            Exception ex, HttpHeaders headers,
-            HttpStatus status, WebRequest request) {
-
+    protected ResponseEntity<Object> handleHttpClientErrorException(
+            HttpClientErrorException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
         return buildResponseEntity(ex, headers, status, request);
     }
 
-    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(
+    protected ResponseEntity<Object> handleHttpMediaTypeException(
+            HttpMediaTypeException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        return buildResponseEntity(ex, headers, status, request);
+    }
+
+    protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptableException(
+            HttpMediaTypeNotAcceptableException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        return buildResponseEntity(ex, headers, status, request);
+    }
+
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupportedException(
             HttpMediaTypeNotSupportedException ex,
             HttpHeaders headers,
             HttpStatus status,
             WebRequest request) {
+        return buildResponseEntity(ex, headers, status, request);
+    }
 
+    protected ResponseEntity<Object> handleHttpMessageConversionException(
+            HttpMessageConversionException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
         return buildResponseEntity(ex, headers, status, request);
     }
 
@@ -51,25 +77,38 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
             HttpHeaders headers,
             HttpStatus status,
             WebRequest request) {
-
         return buildResponseEntity(ex, headers, status, request);
     }
 
-    protected ResponseEntity<Object> handleJsonParseException(
-            JsonParseException ex,
+    protected ResponseEntity<Object> handleHttpMessageNotWritableException(
+            HttpMessageNotWritableException ex,
             HttpHeaders headers,
             HttpStatus status,
             WebRequest request) {
-
         return buildResponseEntity(ex, headers, status, request);
     }
 
-    protected ResponseEntity<Object> ValidationException(
-            ValidationException ex,
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupportedException(
+            HttpRequestMethodNotSupportedException ex,
             HttpHeaders headers,
             HttpStatus status,
             WebRequest request) {
+        return buildResponseEntity(ex, headers, status, request);
+    }
 
+    protected ResponseEntity<Object> handleHttpServerErrorExceptionException(
+            HttpServerErrorException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        return buildResponseEntity(ex, headers, status, request);
+    }
+
+    protected ResponseEntity<Object> handleHttpStatusCodeException(
+            HttpStatusCodeException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
         return buildResponseEntity(ex, headers, status, request);
     }
 
@@ -79,14 +118,8 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
             HttpStatus status,
             WebRequest request) {
 
-        ResponseEntity<?> responseEntity = new ResponseEntity<>(status);
-
-        HashMap<String, String> jsonResponse = new HashMap<>();
-        jsonResponse.put("timestamp", new Date().toString());
-        jsonResponse.put("status", responseEntity.getStatusCode().toString());
-        jsonResponse.put("message", ex.getMessage());
-        jsonResponse.put("path", request.getDescription(false));
-
-        return new ResponseEntity<>(jsonResponse, status);
+        ServletWebRequest servletWebRequest = (ServletWebRequest) request;
+        return new ResponseEntity<>(servletWebRequest.getNativeRequest(), status);
     }
+
 }
