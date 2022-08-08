766f1122f2f1c02135ca0b9b1b9f5d4c25d5d34d
Wagner Galvão <galvao.wagner@outlook.com.br>
2022-08-07 22:31:26 -0300

    class 65 Person project separation

--- src/main/java/br/com/wgalvao/springbootleandrocosta/exceptions/UnsuportedMathOperationException.java
+++ src/main/java/br/com/wgalvao/springbootleandrocosta/exceptions/UnsuportedMathOperationException.java
@@ -1,6 +1,9 @@
 package br.com.wgalvao.springbootleandrocosta.exceptions;
 
-//@ResponseStatus(HttpStatus.BAD_REQUEST)
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class UnsuportedMathOperationException extends RuntimeException {
 
     public UnsuportedMathOperationException(String message) {
